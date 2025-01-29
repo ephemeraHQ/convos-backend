c54e2b77d564319d676a6ca1faa64d0be24a4156
Ry Racherbaumer <ry@xmtp.com>
2025-01-29 15:17:54 -0600

    Add helmet middleware

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import express, {
   type Request,
   type Response,
 } from "express";
+import helmet from "helmet";
 
 const app = express();
 app.use(cors());
+app.use(helmet());
 app.use(express.json({ limit: "50mb" }));
 
 const port = process.env.PORT || 4000;
