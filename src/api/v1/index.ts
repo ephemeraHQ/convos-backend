6c9cc48ab8c1f40064179f71870f73c89e788266
Ry Racherbaumer <ry@xmtp.com>
2025-02-19 09:09:57 -0600

    Add /authenticate to router

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import authenticateRouter from "@/api/v1/authenticate";
 import metadataRouter from "@/api/v1/metadata";
 import profilesRouter from "@/api/v1/profiles/profiles.router";
+import { authMiddleware } from "@/middleware/auth";
 import attachmentsRouter from "./attachments";
 import devicesRouter from "./devices";
 import identitiesRouter from "./identities";
@@ -9,25 +11,28 @@ import usersRouter from "./users";
 
 const v1Router = Router();
 
+// mount authenticate routes under /authenticate
+v1Router.use("/authenticate", authenticateRouter);
+
 // mount user routes under /users
-v1Router.use("/users", usersRouter);
+v1Router.use("/users", authMiddleware, usersRouter);
 
 // mount device routes under /devices
-v1Router.use("/devices", devicesRouter);
+v1Router.use("/devices", authMiddleware, devicesRouter);
 
 // mount identity routes under /identities
-v1Router.use("/identities", identitiesRouter);
+v1Router.use("/identities", authMiddleware, identitiesRouter);
 
 // mount profile routes under /profiles
-v1Router.use("/profiles", profilesRouter);
+v1Router.use("/profiles", authMiddleware, profilesRouter);
 
 // mount metadata routes under /metadata
-v1Router.use("/metadata", metadataRouter);
+v1Router.use("/metadata", authMiddleware, metadataRouter);
 
 // mount lookup routes under /lookup
-v1Router.use("/lookup", lookupRouter);
+v1Router.use("/lookup", authMiddleware, lookupRouter);
 
 // mount attachments routes under /attachments
-v1Router.use("/attachments", attachmentsRouter);
+v1Router.use("/attachments", authMiddleware, attachmentsRouter);
 
 export default v1Router;
