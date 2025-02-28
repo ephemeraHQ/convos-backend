dcf7e58d402730cddfda8de2d1c15ac863bd9763
Thierry <skodathierry@gmail.com>
2025-02-27 20:18:21 -0500

    feat: Minimum version now 2.2.34

--- src/api/v1/appConfig.ts
+++ src/api/v1/appConfig.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const appConfigRouter = Router();
 
 appConfigRouter.get("/", (req, res) => {
-  res.json({ minimumAppVersion: { ios: "2.2.29", android: "2.2.29" } });
+  res.json({ minimumAppVersion: { ios: "2.2.34", android: "2.2.34" } });
 });
 
 export default appConfigRouter;
