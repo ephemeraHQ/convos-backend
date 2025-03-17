253f01c44dba3cedfe53896010c2e405a60fdea3
Louis Rouffineau <dev@louis.io>
2025-03-17 16:49:35 +0100

    Fix double use of rate limiting middleware

--- src/api/v1/profiles/public.router.ts
+++ src/api/v1/profiles/public.router.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { rateLimitMiddleware } from "@/middleware/rateLimit";
 import { getPublicProfile } from "./handlers/get-public-profile";
 
 const publicProfilesRouter = Router();
 
 // Define public routes
-publicProfilesRouter.get("/:username", rateLimitMiddleware, getPublicProfile);
+publicProfilesRouter.get("/:username", getPublicProfile);
 
 export default publicProfilesRouter;
