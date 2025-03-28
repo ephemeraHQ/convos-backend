2a8cc04b19dafdac20044de4651fd6ff06ea9a24
Thierry <skodathierry@gmail.com>
2025-03-27 20:27:20 -0400

    wip

--- src/notifications/utils.ts
+++ src/notifications/utils.ts
@@ -2,5 +2,5 @@ export function getHttpDeliveryNotificationAuthHeader() {
   if (!process.env.XMTP_NOTIFICATION_SECRET) {
     throw new Error("XMTP_NOTIFICATION_SECRET is not set");
   }
-  return `Authorization:Bearer ${process.env.XMTP_NOTIFICATION_SECRET}`;
+  return process.env.XMTP_NOTIFICATION_SECRET;
 }
