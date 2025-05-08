3a8363e58a92202eacafb8b57d19427503c1b854
Louis Rouffineau <dev@louis.io>
2025-05-08 16:24:09 +0200

    Put backward compat route last

--- src/api/v1/profiles/profiles-public.router.ts
+++ src/api/v1/profiles/profiles-public.router.ts
@@ -4,8 +4,8 @@ import { getPublicProfile } from "./handlers/get-public-profile";
 const publicProfilesRouter = Router();
 
 // Define public routes
-publicProfilesRouter.get("/:username", getPublicProfile);
 publicProfilesRouter.get("/username/:username", getPublicProfile);
 publicProfilesRouter.get("/xmtpId/:xmtpId", getPublicProfile);
+publicProfilesRouter.get("/:username", getPublicProfile);
 
 export default publicProfilesRouter;
