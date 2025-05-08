e7dc05c4d1240e393d0eda8b6274c4aa24d35f03
Louis Rouffineau <lourou@users.noreply.github.com>
2025-05-08 16:41:54 +0200

    Get profile by `xmtpId` or `username` (#65)
    /api/v1/profiles/public/:username
    /api/v1/profiles/public/username/:username
    /api/v1/profiles/public/xmtpId/:xmtpId

--- src/api/v1/profiles/profiles-public.router.ts
+++ src/api/v1/profiles/profiles-public.router.ts
@@ -4,6 +4,8 @@ import { getPublicProfile } from "./handlers/get-public-profile";
 const publicProfilesRouter = Router();
 
 // Define public routes
+publicProfilesRouter.get("/username/:username", getPublicProfile);
+publicProfilesRouter.get("/xmtpId/:xmtpId", getPublicProfile);
 publicProfilesRouter.get("/:username", getPublicProfile);
 
 export default publicProfilesRouter;
