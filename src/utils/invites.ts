0cc0e397f8cf73871744ecb76e225de83cb17de1
Louis Rouffineau <dev@louis.io>
2025-07-18 17:40:04 +0200

    Check if `WEBSITE_URL` is set

--- src/utils/invites.ts
+++ src/utils/invites.ts
@@ -1,3 +1,9 @@
+import { AppError } from "./errors";
+
 export const getInviteLink = (inviteId: string): string => {
+  // Check environment variables
+  if (!process.env.WEBSITE_URL) {
+    throw new AppError(500, "WEBSITE_URL is not set");
+  }
   return `${process.env.WEBSITE_URL}/join/${inviteId}`;
 };
