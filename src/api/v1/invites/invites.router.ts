41d4a5c177f89ae9f93c5a989c9b66db2fd6fe02
Louis Rouffineau <dev@louis.io>
2025-07-21 17:20:27 +0200

    feat: complete invite code details (authenticated endpoint)

--- src/api/v1/invites/invites.router.ts
+++ src/api/v1/invites/invites.router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { createInviteCode } from "./handlers/create-invite-code";
+import { getAuthenticatedInviteDetailsHandler } from "./handlers/get-invite-details";
 import { getInviteRequests } from "./handlers/get-invite-requests";
 import { requestToJoin } from "./handlers/request-to-join";
 
@@ -8,5 +9,7 @@ const invitesRouter = Router();
 invitesRouter.post("/", createInviteCode);
 invitesRouter.post("/request", requestToJoin);
 invitesRouter.get("/requests", getInviteRequests);
+invitesRouter.post("/:inviteId", createInviteCode);
+invitesRouter.get("/:inviteId", getAuthenticatedInviteDetailsHandler);
 
 export default invitesRouter;
