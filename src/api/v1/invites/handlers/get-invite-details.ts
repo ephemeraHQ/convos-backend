7416f82ef4c01915f3002632470da8746ce51cf0
Noé Malzieu <noe.malzieu@gmail.com>
2025-08-27 10:24:26 +0200

    Correct chain

--- src/api/v1/invites/handlers/get-invite-details.ts
+++ src/api/v1/invites/handlers/get-invite-details.ts
@@ -225,7 +225,7 @@ export const getAuthenticatedInviteDetailsHandler = async (
       },
     });
 
-    if (!invite || !invite.createdBy?.xmtpId) {
+    if (!invite || !invite.createdBy.xmtpId) {
       res.status(404).json({
         success: false,
         message: "Invite not found",
