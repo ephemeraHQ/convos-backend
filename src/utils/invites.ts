b07fc9508b6f34b4ed2292aa5172054a96f4be4e
Louis Rouffineau <dev@louis.io>
2025-09-12 17:56:46 +0200

    Change join invite URL scheme

--- src/utils/invites.ts
+++ src/utils/invites.ts
@@ -1,4 +1,4 @@
 export const getInviteLink = (inviteId: string): string => {
   const baseUrl = process.env.WEBSITE_URL || "";
-  return `${baseUrl}/join/${inviteId}`;
+  return `${baseUrl}/${inviteId}`;
 };
