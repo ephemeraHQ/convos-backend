e9d03eb70be5ca0ebd88f57cb9598cd4e0102cc2
Louis Rouffineau <dev@louis.io>
2025-10-03 19:27:26 +0200

    Data model migration

---
+++ prisma/migrations/20251003172324_add_v2_push_notification_models/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "PushTokenRegistration" (
+    "deviceId" TEXT NOT NULL,
+    "pushToken" TEXT NOT NULL,
+    "tokenType" "PushTokenType" NOT NULL DEFAULT 'apns',
+    "apnsEnv" "ApnsEnvironment",
+    "pushFailures" INTEGER NOT NULL DEFAULT 0,
+    "disabled" BOOLEAN NOT NULL DEFAULT false,
+    "addedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "lastSentAt" TIMESTAMP(3),
+    "lastFailureAt" TIMESTAMP(3),
+
+    CONSTRAINT "PushTokenRegistration_pkey" PRIMARY KEY ("deviceId")
+);
+
+-- CreateTable
+CREATE TABLE "ClientIdentifier" (
+    "id" TEXT NOT NULL,
+    "deviceId" TEXT NOT NULL,
+    "addedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ClientIdentifier_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "PushTokenRegistration_pushToken_idx" ON "PushTokenRegistration"("pushToken");
+
+-- CreateIndex
+CREATE INDEX "ClientIdentifier_deviceId_idx" ON "ClientIdentifier"("deviceId");
+
+-- AddForeignKey
+ALTER TABLE "ClientIdentifier" ADD CONSTRAINT "ClientIdentifier_deviceId_fkey" FOREIGN KEY ("deviceId") REFERENCES "PushTokenRegistration"("deviceId") ON DELETE CASCADE ON UPDATE CASCADE;
