93047d0374ba4fd74ea93fdea9dfb7eb058c0680
Borja Aranda <borja@xmtp.com>
2026-05-08 14:11:03 +0200

    feat(auth): add Account, AuthMethod, AuthNonce models

---
+++ prisma/migrations/20260508120812_authentication_api/migration.sql
@@ -0,0 +1,41 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- CreateEnum
+CREATE TYPE "AuthMethodType" AS ENUM ('SIWE');
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AuthMethod" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "accountId" UUID NOT NULL,
+    "type" "AuthMethodType" NOT NULL,
+    "externalKey" TEXT NOT NULL,
+    "addedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AuthMethod_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AuthNonce" (
+    "nonce" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AuthNonce_pkey" PRIMARY KEY ("nonce")
+);
+
+-- CreateIndex
+CREATE INDEX "AuthMethod_accountId_idx" ON "AuthMethod"("accountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AuthMethod_type_externalKey_key" ON "AuthMethod"("type", "externalKey");
+
+-- AddForeignKey
+ALTER TABLE "AuthMethod" ADD CONSTRAINT "AuthMethod_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
