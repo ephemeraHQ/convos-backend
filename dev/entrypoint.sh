10c81a8629ed1169d0e9897908955ef2ce25d52c
Borja Aranda <borja@xmtp.com>
2026-05-22 16:57:47 +0200

    fix(docker): drop --env-file-if-exists from container entrypoint (#250)

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -6,7 +6,6 @@ node ./node_modules/prisma/build/index.js migrate deploy
 
 exec node \
   --enable-source-maps \
-  --env-file-if-exists=.env \
   --import @opentelemetry/instrumentation/hook.mjs \
   --import file:///app/dist/instrumentation.js \
   /app/dist/index.js
