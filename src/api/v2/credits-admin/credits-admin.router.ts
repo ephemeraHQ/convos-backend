6be4233fd5db6ba5422d8bf8f5351f0ae61105fe
Borja Aranda <borja@xmtp.com>
2026-06-17 19:18:08 +0200

    feat(credits-admin): GET /search account lookup

--- src/api/v2/credits-admin/credits-admin.router.ts
+++ src/api/v2/credits-admin/credits-admin.router.ts
@@ -1,3 +1,7 @@
 import { Router } from "express";
+import { searchGetHandler } from "./handlers/search-get";
+import { cfAccessHeaderMiddleware } from "./middleware/cf-access";
 
 export const creditsAdminRouter = Router();
+
+creditsAdminRouter.get("/search", cfAccessHeaderMiddleware, searchGetHandler);
